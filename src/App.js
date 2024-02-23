c719f3d9aba08da6700785a6ac4e4f68cb3cbfd2
chiranjeebnayak <chiranjeeb.nayak@juspay.in>
2024-02-23 16:39:52 +0530

    pipline testing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
     <>
       <h1>My Website</h1>
       <p>Welcome to CodeMap</p>
+      <p>Chiranjeeb Nayak, Founder</p>
+      <p>CI/CD pipeline Testing - Done</p>
     </>
   );
 }
